2af393b80c53660261a4eea75c0e98ea63fa5431
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-10-10 16:28:17 +0200

    Add files via upload

---
+++ 05 - Readable Streams/script.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const fs = require("fs");
+
+let settings = {
+  input: fs.createReadStream("./shoppingList.txt"),
+};
+
+const myInterface = readline.createInterface(settings);
+
+const printData = (data) => {
+  console.log(`Item: ${data}`);
+};
+
+myInterface.on("line", printData);
